7ac304ac920acdd785978ca009666d44cf2df13f
OnuCameliaW <onu.camelia@yahoo.com>
2017-07-22 14:39:16 +0300

    Add start-up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angAppApp
+ */
+angular.module('angAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
